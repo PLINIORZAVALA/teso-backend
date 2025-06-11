cf7d1f91caeaef84bec138816af54f435066dc5f
PLINIORZAVALA <plmax3543@gmail.com>
2025-06-11 15:48:30 -0500

    Configuración final de proceso paralelo

--- main.py
+++ main.py
@@ -28,9 +28,9 @@ if __name__ == '__main__':
     p2 = Process(target=run_sede_b)
     
     # Iniciar los hilos
-    t1.start()
-    t2.start()
+    p1.start()
+    p2.start()
 
     # Esperar a que ambos terminen
-    t1.join()
-    t2.join()
+    p1.join()
+    p2.join()
