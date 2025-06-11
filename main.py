8963bb3562f46f8bab4b36cc2caf69809ee2ea1c
PLINIORZAVALA <plmax3543@gmail.com>
2025-06-11 15:46:04 -0500

    Se añadio la configuracion para recibir la peticiones mediante hilos y en paralelo

--- main.py
+++ main.py
@@ -1,19 +1,32 @@
-import threading
+from multiprocessing import Process
 from app import create_app
 
 def run_sede_a():
     app_a = create_app('config/config_sede_a.py')
-    app_a.run(port=5001, debug=True, use_reloader=False)  # importante desactivar use_reloader
+    app_a.run(port=5001, debug=True, use_reloader=False) 
 
 def run_sede_b():
     app_b = create_app('config/config_sede_b.py')
     app_b.run(port=5002, debug=True, use_reloader=False)
 
 if __name__ == '__main__':
-    # Crear los hilos
+    
+    """
+    # PROCESO HACIENDO USO DE PROCESOS HACIENDO USO DE LOS HILOS
+    import threading
     t1 = threading.Thread(target=run_sede_a)
     t2 = threading.Thread(target=run_sede_b)
 
+    # PROCESO HACIENDO USO DE PROCESOS HACIEDO USO DEL PARALELISMO DE PROCESOS
+    from multiprocessing import Process
+    p1 = Process(target=run_sede_a)
+    p2 = Process(target=run_sede_b)
+    """
+
+    # Proceso en sedes a y b
+    p1 = Process(target=run_sede_a)
+    p2 = Process(target=run_sede_b)
+    
     # Iniciar los hilos
     t1.start()
     t2.start()
